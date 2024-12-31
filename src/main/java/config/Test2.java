8c14b80214b6a851870b2a217f57801898885253
Jenila <jenila0722@gmail.com>
2024-12-31 17:25:19 +0000

    Commiting Test 2 to Branch X

---
+++ src/main/java/config/Test2.java
@@ -0,0 +1,18 @@
+package config;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String hungry ="yes";
+		if (hungry =="yes")
+		{
+			System.out.println("She is hungry");
+		}
+		else
+		{
+			System.out.println("She is not hungry");
+		}
+
+	}
+
+}
